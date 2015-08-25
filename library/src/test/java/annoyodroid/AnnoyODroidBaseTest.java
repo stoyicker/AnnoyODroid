06025a7dc016ef1fea5caf533b16ad1d46bfb0fe
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2015-08-25 20:21:19 +0200

    Added a dummy test

---
+++ library/src/test/java/annoyodroid/AnnoyODroidBaseTest.java
@@ -0,0 +1,16 @@
+package annoyodroid;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class AnnoyODroidBaseTest {
+
+    @Test
+    public void dummyTest() {
+        Assert.assertEquals(2 + 2, 4);
+    }
+}
